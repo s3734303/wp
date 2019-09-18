febf118adad143d41c9930af96f9e8de3b82faaa
Home <harvindlason@Macbook-Pro.local>
2019-09-18 19:50:07 +1000

    assignment 3 initialized

--- a3/index.php
+++ a3/index.php
@@ -3,38 +3,224 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible">
     <title>Assignment 2</title>
-    
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
-    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
-    <script src='../wireframe.js'></script>
+   <!--<link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>	-->
+    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css">
+	  
+<script src='../wireframe.js'></script>
+	  <script src='javascript.js'></script>
   </head>
 
-  <body>
+  <body  style="background-color: "#F2F2F2">
 
-    <header>
-      <div>Put company logo and name here</div>
-    </header>
+  <header>
+	  <div>
+		  <div id="nav-logo" class="header">
+		<h1>Cinema</h1>
+		<a href="index.php"><img src="../../media/logo.png" alt="logo" ></a>
+	    </div>
+		  
+		  
+		  
+	  <nav> 
+		  <div id="navbar">
+		 	 <a href="#aboutus" class="active">About Us</a> </li>
+	  		<a href="#prices">Prices</a> </li>      
+	 		<a href="#nowshowing">Now Showing</a></li>
+	  </div>
+      
+  </nav>
+	  </div>   
+  </header>
+<main>
+  <section id="aboutus">
+	<div class="container" class="content"><video	width="100%" autoplay muted playsinline><source src="https://ak4.picdn.net/shutterstock/videos/1015060114/preview/stock-footage-smiling-woman-watching-movie-close-up-of-beautiful-girl-laughing-in-cinema-in-slow-motion.mp4">
+	  </video>
+	  <div class="centered">
+		  <h2>CinemaHub</h2>
+		  <h1>Now Repened.</h1>
+		  <h2>We just spend a large chunk of money on renovation, come and see for yourself.</h2>
+	  </div>	
+    </div>
+	<div >
+		<h2>Seat Option: standard seat</h2>
+	</div>
+	<div id="chair">
+		<div>
+			<h3>&#34;I&prime;m gonna actually spending money on a movie, but the budget option will be fine&#34;, said no one ever.</h3>
+		<img src="../../media/standard.png" alt=""/>
+	</div>
+	<div>
+		<h2 >Seat Option: First Class seat</h2>
+	</div>
+	<div>
+			<h3>This one. Obviously.</h3>
+		<img src="../../media/firstclass.png" alt=""/>
+	</div>				   
+	</div>
+	
+	
+							
+<div class="parallax container">
+	  <div class="top-left">
+	    <h2 align="left"> Now with Dolby Vision™</h3>
+	    <h5 align="left">Dolby Vision™ was designed for the cinema to deliver incredible color, a million-to-one contrast ratio, and twice the brightness of standard screens. TL;DR: It makes movies look good.</h5>
+	  </div>
+    </div>
+	<div class="container"><img src="https://www.dolby.com/uploadedImages/wwwdolbycom/Content/Platforms/Dolby_Cinema/Dolby_Cinema_Platform_Page/2_MovingAudio_TextRight_V2.jpg?resize=3240:1266" width="100%">
+	  <div class="top-right">
+		  <h2 align="right" >Enjoy new immersive experience </h2>
+		  <h5 align="right">Dolby Atmos® is an industry-leading audio technology that delivers powerful, moving audio that flows all around you. Now available in CinemaHub.
+		  </h5>
+		</div>
+    </div>
+  </section>
+  
+<section id="prices">
+	<h1>Prices</h1>
+	<div class="grid-container">
+    <div class="grid-item">Seat Type</div>
+    <div class="grid-item">Seat Code</div>
+    <div class="grid-item">All day Monday and Wednesday AND 12pm on Weekdays</div>
+    <div class="grid-item">All other times</div>
+    <div class="grid-item">Standard Adult</div>
+    <div class="grid-item">STA</div>
+    <div class="grid-item">14.00</div>
+    <div class="grid-item">19.80</div>
+    <div class="grid-item">Standard Concession</div>
+    <div class="grid-item">STP</div>
+    <div class="grid-item">12.50</div>
+    <div class="grid-item">17.50</div>
+    <div class="grid-item">Standard Child</div>
+    <div class="grid-item">STC</div>
+    <div class="grid-item">11.00</div>
+    <div class="grid-item">15.30</div>
+    <div class="grid-item">First Class Adult</div>
+    <div class="grid-item">FCA</div>
+	<div class="grid-item">24.00</div>
+	<div class="grid-item">30.00</div>
+	<div class="grid-item">First Class Concession</div>
+	<div class="grid-item">FCC</div>
+	<div class="grid-item">22.50</div>
+	<div class="grid-item">27.00</div>
+	<div class="grid-item">First Class Child</div>
+	<div class="grid-item">FCC</div>
+	<div class="grid-item">21.00</div>
+	<div class="grid-item">24.00</div>	  
+  </div>
+</section>
+<section id="nowshowing">
+	<div><h1>Now showing</h1></div>
+<div class="postblock">
+	  <div class="fixsize"><img src="../../media/avenger.jpg" alt="avenger"/></div>
+	  <div class="timetable">
+			<h2>Avenger: End Game</h2>
+			<h4>G</h4>
+			<h5>Wed - 9pm</h5>
+			<h5>Thu - 9pm</h5>
+			<h5>Fri - 9pm</h5>
+			<h5>Sat - 6pm</h5>
+			<h5>Sun - 6pm</h5>
+			
+		</div>
+    </div>
+<div class="postblock">
+	<div class="fixsize"><img src="../../media/topendwedding.jpg" alt=""/></div>
+	<div class="timetable">
+        <h2>Top End Wedding</h2>
+		<h4>G</h4>
+        <h5>Mon - 6pm</h5>
+        <h5>Tue - 6pm</h5>
+        <h5>Sat - 3pm</h5>
+        <h5>Sun - 3pm</h5>
+					  
+	</div>
+</div>
+														
+<div class="postblock">
+	<div class="fixsize"><img src="../../media/dumbo.jpg"  alt="dumbo"/></div>
+	<div class="timetable">
+          <h2>Dumbo</h2>
+			<h4>G</h4>
+          <h5>Mon - 12pm</h5>
+          <h5>Tue - 12pm</h5>
+          <h5>Wed - 6pm</h5>
+          <h5>Thu - 6pm</h5>
+          <h5>Fri - 6pm</h5>
+          <h5>Sat - 3pm</h5>
+          <h5>Sun - 3pm</h5>
+		  </div>
+</div>
+	<div class="postblock">
+		<div class="fixsize"><img src="../../media/thehappyprince.jpg" alt=""/></div>
+	  <div class="timetable">
+				<h2>The Happy Prince</h2>
+				<h4>G</h4>
+				<h5>Wed - 12pm</h5>
+				<h5>Thu - 12pm</h5>
+				<h5>Fri - 12pm</h5>
+				<h5>Sat - 9pm</h5>
+				<h5>Sun - 9pm</h5>
+				
 
-    <nav>
-      <div>Put website navigation links here</div>
-    </nav>
-
-    <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
-    </main>
+	</div>
+</div>	
+</section>
+<section id="synopsis">
+	<h1>Avenger: End Game&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; PG</h1>						   
+    <div>
+      
+    
+	<div  class="synopsis">
+  		<div id="#plot">
+			<h3>Plot Description:</h3>
+			<h4>After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe. </h4>
+    	</div>
+		<div id="trailer">
+			<iframe width="640" height="480" src="https://www.youtube.com/embed/TcMBFSGVi1c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+		</div>
+	</div>
+    <div>
+		<h2>Make a booking:</h2>
+		<button type="button">Wed-9pm</button>
+		<button type="button">Thu-9pm</button>
+		<button type="button">Fri-9pm</button>
+		<button type="button">Sat-6pm</button>
+		<button type="button">Sun-6pm</button>
+	</div>
+</section>
+</main>
 
     <footer>
-      <div>&copy;<script>
+<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
-      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+      </script> 
+      Shiou-Ping Chu s3734303 Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      <div>
+        <p>Disclaimer: This website is not a real website and is being developed as part of a Schoolof Science Web Programming course at RMIT University in Melbourne, Australia.</p>
+      </div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
-    </footer>
+  </footer>
 
-  </body>
+
+<script>
+
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+</script>
+
+
+</body>
 </html>
